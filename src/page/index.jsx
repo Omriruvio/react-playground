55d3d8b7014a64081a8abadf46133ec96f10e113
Omri Ruvio <omriruvio@gmail.com>
2022-03-25 16:01:51 +0300

    initial commit

---
+++ src/page/index.jsx
@@ -0,0 +1,12 @@
+const styles = {
+  width: '80%',
+  backgroundColor: 'white',
+  textAlign: 'center'
+}
+
+ReactDOM.render(
+  (<div style={styles}>
+    <h1 className="title">Hello, world!</h1>
+  </div>),
+  document.querySelector('#root'),
+);
